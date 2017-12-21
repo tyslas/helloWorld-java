b3b170ef1cf40b21bbe031c2b81d4d095ed3034c
Tito Yslas <roberto.yslas@gmail.com>
2017-12-21 11:47:50 -0700

    intro to ints

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,19 @@
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello, world!");
+
+        int myFirstInt = (10 - 5) * 4 / 5;
+        int mySecondInt = 12;
+        int myThirdInt = 3;
+
+        int myTotal = myFirstInt + mySecondInt + myThirdInt;
+        int difference = 1000 - myTotal;
+
+        System.out.println("myFirstInt: " + myFirstInt);
+        System.out.println("mySecondInt: " + mySecondInt);
+        System.out.println("myThirdInt: " + myThirdInt);
+        System.out.println("myTotal: " + myTotal);
+        System.out.println("difference: " + difference);
     }
 }
